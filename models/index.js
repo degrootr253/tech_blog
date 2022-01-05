7f823c46486a596008576d96fe2f0092a6b1e8b8
Ryan DeGroot <degrootr253@gmail.com>
2022-01-05 12:38:05 -0800

    complete index of models

---
+++ models/index.js
@@ -0,0 +1,33 @@
+const Comment = require('./Comment');
+const User = require('./User');
+const Post = require('./Post');
+
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+})
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id',
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id',
+    onDelete: 'CASCADE'
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id',
+    onDelete: 'CASCADE'
+});
+
+module.exports = { Comment, User, Post };
