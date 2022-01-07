46953075971fdda0b57c116fba7a78194c14aa88
Ryan DeGroot <degrootr253@gmail.com>
2022-01-06 17:02:01 -0800

    finishing add posts front and backend js

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const { Post } = require('../../models');
 const withAuth = require('../../utils/auth');
 
-router.post('/', withAuth, async (req, res) => {
+router.post('/post', withAuth, async (req, res) => {
     try {
         const newPost = await Post.create({
             ...req.body,
@@ -15,29 +15,25 @@ router.post('/', withAuth, async (req, res) => {
     }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/post/:id', (req, res) => {
     try {
-      const postDB = await Post.findAll({
-        where: {
-          id: req.body.id
-        }
-      });
-      res.status(200).json(postDB);
+        const postDB = await Post.findOne({id: req.body.id}, (err) => if(err){console.log(err)});
+        res.status(200).json(postDB);
     } catch (err) {
-      res.status(400).json(err);
+        res.status(400).json(err);
     }
-  });
+});
 
-  router.get('/', (req, res) => {
+router.get('/post', (req, res) => {
     try {
-      const postDB = await Post.findAll({});
-      res.status(200).json(postDB);
+        const postDB = await Post.findAll({});
+        res.status(200).json(postDB);
     } catch (err) {
-      res.status(400).json(err);
+        res.status(400).json(err);
     }
-  });
+});
 
-router.put('/:id', withAuth, async (req, res) => {
+router.put('post/:id', withAuth, async (req, res) => {
     try {
         const postData = await Post.update({
             title: req.body.title,
@@ -60,7 +56,7 @@ router.put('/:id', withAuth, async (req, res) => {
     }
 });
 
-router.delete('/:id', withAuth, async (req, res) => {
+router.delete('post/:id', withAuth, async (req, res) => {
     try {
         const postData = await Post.destroy({
             where: {
@@ -81,3 +77,5 @@ router.delete('/:id', withAuth, async (req, res) => {
 });
 
 module.exports = router;
+
+// req.session.user_id
--- public/javascript/signup.js
+++ public/javascript/signup.js
@@ -1,13 +1,13 @@
 const signupFormHandler = async (event) => {
     event.preventDefault();
   
-    const name = document.querySelector('#username-signup').value.trim();
+    const username = document.querySelector('#username-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
   
     if (name && password) {
       const response = await fetch('/api/users', {
         method: 'POST',
-        body: JSON.stringify({ name, password }),
+        body: JSON.stringify({ username, password }),
         headers: { 'Content-Type': 'application/json' },
       });
   
